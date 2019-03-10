97ca690c5a9e03112097d8f58b7f35658c85b0b7
Yucheng Yin <yyucheng@umich.edu>
2019-03-10 19:57:54 -0400

    modify setup_general

--- server_setup/dns/setup_general.sh
+++ server_setup/dns/setup_general.sh
@@ -25,34 +25,15 @@ sudo apt-get --assume-yes install vim
 sudo atp-get --assume-yes install tmux
 #sudo apt-get --assume-yes remove ntp ntpdate
 sudo apt-get --assume-yes install build-essential automake
-#wget https://www.eecis.udel.edu/~ntp/ntp_spool/ntp4/ntp-4.2/${NTPVER}.tar.gz
-#tar xfz ${NTPVER}.tar.gz
-#pushd ${NTPVER}
-#./configure
-#sudo make install
-#popd
-#sudo cp ${INITD_SCRIPT} /etc/init.d/ntp
-#sudo cp ${NTPCONF} /etc/ntp.conf
 
 
-#echo 'restrict default nomodify notrap nopeer noquery' | sudo tee --append /etc/ntp.conf
-#echo 'restrict '${LOCAL_IP}' mask '${NET_MASK} | sudo tee --append /etc/ntp.conf
+# sudo apt-get --assume-yes install python-pip python-numpy
+# sudo apt-get --assume-yes install  python-numpy
+# sudo apt-get --assume-yes install  python-scapy
+# sudo apt-get --assume-yes install  python-yaml
+# sudo pip install dnspython
+# sudo pip install numpy-indexed
 
-#sudo service ntp restart
-
-git clone https://github.com/secdev/scapy
-#git clone https://@bitbucket.org/ankitjena/ampmap_nmap.git
-
-sed -ie 's/r.answers/True or r.answers/g' scapy/scapy/sendrecv.py
-pushd scapy
-sudo -H python setup.py install
-popd
-sudo apt-get --assume-yes install python-pip python-numpy
-sudo apt-get --assume-yes install  python-numpy
-sudo apt-get --assume-yes install  python-scapy
-sudo apt-get --assume-yes install  python-yaml
-sudo pip install dnspython
-sudo pip install numpy-indexed
 
 sudo apt-get --assume-yes install python3-pip
 sudo apt-get --assume-yes install python3-numpy
@@ -61,7 +42,17 @@ sudo apt-get --assume-yes install python3-matplotlib
 sudo apt-get --assume-yes install python3-dnspython
 sudo apt-get --assume-yes install python3-sqlalchemy
 sudo apt-get --assume-yes install python3-psycopg2
-sudo pip3  install scapy 
+sudo apt-get --assume-yes install python3-setuptools
+
+sudo pip3 install --upgrade setuptools
+
+git clone https://github.com/secdev/scapy
+sed -ie 's/r.answers/True or r.answers/g' scapy/scapy/sendrecv.py
+pushd scapy
+sudo -H python3 setup.py install
+popd
+
+#sudo pip3  install scapy 
 sudo pip3 install --upgrade numpy
 sudo apt-get --assume-yes install python3-pandas
 sudo pip3 install -U scikit-learn
